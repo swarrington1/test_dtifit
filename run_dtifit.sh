a619c0844ce8558e3d69379365245feab4ea4dbe
Shaun Warrington <shaun.warrington1@nottingham.ac.uk>
2025-10-01 15:17:31 -0500

    dtifit test app

--- run_dtifit.sh
+++ run_dtifit.sh
@@ -13,4 +13,4 @@ fslmaths dti_L2.nii.gz -add dti_L3.nii.gz -div 2 ./tensor/rd.nii.gz;
 cp -v dti_FA.nii.gz ./tensor/fa.nii.gz
 cp -v dti_MD.nii.gz ./tensor/md.nii.gz
 cp -v dti_tensor.nii.gz ./tensor/tensors.nii.gz
-cp -v dti_L1.nii.gz ./tensor/ad.nii.gz;
+cp -v dti_L1.nii.gz ./tensor/ad.nii.gz
