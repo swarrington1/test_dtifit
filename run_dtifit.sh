80339e1f8094f4fa0ce2f9407ef806c947e6df8a
Shaun Warrington <shaun.warrington1@nottingham.ac.uk>
2025-10-01 15:04:50 -0500

    dtifit test app

--- run_dtifit.sh
+++ run_dtifit.sh
@@ -6,4 +6,4 @@ bvals=`jq -r '.bvals' config.json`;
 bvecs=`jq -r '.bvecs' config.json`;
 mask=`jq -r '.mask' config.json`;
 
-dtifit -k ${dwi} -m ${mask} -o ${out_dir} -r ${bvecs} -b ${bvals} --save_tensor
+dtifit -k ${dwi} -m ${mask} -o dtifit/dti -r ${bvecs} -b ${bvals} --save_tensor
